b74f304f8b7d6bcbe20d4a765422f5330853c4fc
Jhoukx <jegonzalezesparragoza@gmail.com>
2023-08-23 15:58:58 -0500

    feat: :sparkles: Agregué restriccion para enviar la query de especialidad

--- app/controllers/medico.controller.js
+++ app/controllers/medico.controller.js
@@ -6,6 +6,7 @@ const cita = db.collection('medico');
 const getEspecialidad = async (req, res) => {
     try {
         let result = await cita.find({ especialidad: req.query.especialidad }).toArray();
+        if (!req.query.especialidad) return res.status(404).json({ status: 404, message: 'Especialidad not found' })
         res.json(result);
     } catch (error) {
         res.status(404).json({status:404,message:'Especialidad not found'})
