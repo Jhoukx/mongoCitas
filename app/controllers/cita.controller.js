9bce02ed6ea714252f07a93e061896c12b32b966
Jhoukx <jegonzalezesparragoza@gmail.com>
2023-08-23 17:20:13 -0500

    feat: :sparkles: Consulta de cita sobre todos los pacientes de un medico

--- app/controllers/cita.controller.js
+++ app/controllers/cita.controller.js
@@ -20,4 +20,51 @@ const getPaciente = async (req, res) => {
         res.status(404).json({ status: 404, message: 'Couldn´t find a cita from that usuario' })
     }
 } 
-export { getAll,getPaciente}
+
+const getMedico = async (req, res) => {
+    try {
+        let result = await cita.aggregate([
+            {
+                $match: {
+                    medico: Number(req.params.id)
+                }
+            },
+            {
+                $lookup: {
+                    from: 'usuario',
+                    localField: 'medico',
+                    foreignField: 'id',
+                    as: 'Pacientes'
+                }
+            },
+            {
+                $unwind: '$Pacientes'
+            },
+            {
+                $project: {
+                    ID: '$id',
+                    FECHA: '$fecha',
+                    ESTADO: '$estado',
+                    MEDICO: '$medico',
+                    DATOSUSUARIO: '$datosUsuario',
+                    PACIENTES: {
+                        ID: '$Pacientes.id',
+                        NOMBRE: '$Pacientes.nombre',
+                        PRIMER_APELLIDO: '$Pacientes.primer_apellido',
+                        SEGUNDO_APELLIDO: '$Pacientes.segundo_apellido',
+                        TELEFONO: '$Pacientes.telefono',
+                        DIRECCION: '$Pacientes.direccion',
+                        EMAIL: '$Pacientes.email',
+                        TIPODOC: '$Pacientes.tipodoc',
+                        GENERO: '$Pacientes.genero',
+                        ACUDIENTE: '$Pacientes.acudiente'
+                    }
+                }
+            }
+        ]).toArray()
+        res.send(result)
+    } catch (error) {
+        res.status(404).json({ status: 404, message: 'Couldn´t find' })
+    }
+}
+export { getAll,getPaciente,getMedico}
--- app/routes/cita.routes.js
+++ app/routes/cita.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getAll, getPaciente } from "../controllers/cita.controller.js";
+import { getAll, getMedico, getPaciente } from "../controllers/cita.controller.js";
 
 const appCita = Router();
 
 appCita.get('/', getAll);
 appCita.get('/paciente/:id', getPaciente);
+appCita.get('/medico/:id',getMedico);
 
 export default appCita;
